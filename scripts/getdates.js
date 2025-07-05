74cf795426671f0f293478119682044f5787860d
mesh <meshackamadi@gmail.com>
2025-07-05 22:01:29 +0100

    got javascript to work and linked it successfully.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("currentyear").textContent = new Date().getFullYear();
   document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
