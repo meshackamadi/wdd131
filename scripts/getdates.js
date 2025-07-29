9328c05ac0fa4fe5f3d893be4cc041a65de8c7db
mesh <meshackamadi@gmail.com>
2025-07-29 05:00:17 +0100

    reorganized my file

--- scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("currentyear").textContent = new Date().getFullYear();
-  document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
-});
